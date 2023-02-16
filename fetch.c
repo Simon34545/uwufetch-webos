5d8ce1ccd9fe814d339151511e03c81ad89a28c1
simon_34545 <70719312+Simon34545@users.noreply.github.com>
2023-02-16 00:57:34 -0800

    Update fetch.c

--- fetch.c
+++ fetch.c
@@ -670,7 +670,7 @@ void get_info(struct flags flags, struct info* user_info) {
 	#endif
 			}
 #endif
-		} else if (access("/etc/starfish-release", F_OK)) { // webOS
+		} else if (access("/etc/starfish-release", F_OK) == 0) { // webOS
 			sprintf(user_info->os_name, "webos");
 		} else // if no option before is working, the system is unknown
 			sprintf(user_info->os_name, "unknown");
